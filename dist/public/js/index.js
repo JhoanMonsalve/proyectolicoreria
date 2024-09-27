360b73ebcf561bd72c3c125bded3800b3200991a
JhoanMonsalve <jhoanmonsalve320@gmail.com>
2024-09-27 16:05:05 -0500

    funcionalidad en las imagenes

--- dist/public/js/index.js
+++ dist/public/js/index.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const buttonSmirnoff = document.getElementById("ver-smirnoff");
+    const imageSmirnoff = document.querySelector("#smirnoff img");
 
     const modal = document.getElementById("modal-smirnoff");
     const closeModal = document.querySelector(".close");
 
-    
     const productoSmirnoff = {
         title: "Smirnoff",
         image: "/img/cerveza/smirnoff.jpg",
@@ -14,26 +14,31 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     function abrirModal() {
-        
         modal.querySelector("h2").textContent = productoSmirnoff.title;
         modal.querySelector("img").src = productoSmirnoff.image;
         modal.querySelector("p").textContent = productoSmirnoff.description;
-
-        
         modal.style.display = "flex";
     }
 
+    
     buttonSmirnoff.addEventListener("click", function () {
         abrirModal();
     });
 
-    closeModal.addEventListener("click", function () {
-        modal.style.display = "none"; 
+    
+    imageSmirnoff.addEventListener("click", function () {
+        abrirModal();
     });
 
+    
+    closeModal.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    
     window.addEventListener("click", function (event) {
         if (event.target === modal) {
-            modal.style.display = "none"; 
+            modal.style.display = "none";
         }
     });
 
